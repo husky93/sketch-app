246b6c1067d03ce8626b040b4801efa3e25e9e42
Maciej Sroka <sroka.maciej93@gmail.com>
2022-05-09 17:07:06 +0200

    Add drawing capability

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const canvas = document.querySelector('.canvas');
+let isMouseDown = false;
+canvas.addEventListener('mousedown', e => {
+    e.preventDefault();
+    enableMouseDown();
+});
+canvas.addEventListener('mouseup', disableMouseDown);
+
 createCanvas(16);
+addDrawingCapability();
+
+
+function enableMouseDown() {
+    isMouseDown = true;
+}
+
+function disableMouseDown() {
+    isMouseDown = false;
+}
 
 function createCanvas(dimension) {
     for(let i = 0; i < dimension; i++) {
@@ -12,4 +29,18 @@ function createCanvas(dimension) {
             row.appendChild(tile);
         }
     }
+}
+
+function addDrawingCapability() {
+    const tiles = document.querySelectorAll('.tile');
+    tiles.forEach(tile => {
+        tile.addEventListener('mouseover', e => {
+            if(isMouseDown) {
+                tile.style.cssText = 'background-color: red;'
+            }
+        })
+        tile.addEventListener('click', e => {
+            tile.style.cssText = 'background-color: red;'
+        })
+    })
 }
